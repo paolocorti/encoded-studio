5a5c2da0c699f7bd9eb393daec4e680945a24b21
Paolo <paolocorti87@gmail.com>
2022-01-07 21:06:27 +0100

    fix class error

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,29 +2,29 @@ const Header = () => {
 
 
   return (
-    <nav class="bg-white">
-      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div class="relative flex items-center justify-between h-16">
-          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-              <span class="sr-only">Open main menu</span>
+    <nav className="bg-white">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+              <span className="sr-only">Open main menu</span>
 
-              <img class="block lg:hidden h-8 w-auto" src={'/images/logo.png'} alt="Encoded Logo" />
+              <img className="block lg:hidden h-8 w-auto" src={'/images/logo.png'} alt="Encoded Logo" />
 
             </button>
           </div>
-          <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div class="flex-shrink-0 flex items-center">
-              <img class="hidden sm:block h-16 w-auto" src={'/images/logo.png'} alt="Encoded Logo" />
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center">
+              <img className="hidden sm:block h-16 w-auto" src={'/images/logo.png'} alt="Encoded Logo" />
             </div>
-            <div class="hidden sm:block sm:ml-6">
-              <div class="flex space-x-4">
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
 
-                {/* <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
+                {/* <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
 
-                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
 
-                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a> */}
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a> */}
               </div>
             </div>
           </div>
